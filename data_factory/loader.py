f6265c4008a6e56ca83e3efa8a505d9e0c6fff71
JadeLee274 <tmdals274@snu.ac.kr>
2025-08-04 16:14:38 +0900

    update

--- data_factory/loader.py
+++ data_factory/loader.py
@@ -6,19 +6,62 @@ from sklearn.preprocessing import StandardScaler
 Vector = np.ndarray
 Matrix = np.ndarray
 PATH = '/data/seungmin'
+"""
+Codes for loading data. This code follows the paper
+Darban et al., 2025, GenIAS: Generator for Instantiating Anomalies in Time Series.
+
+Paper link: https://arxiv.org/pdf/2502.08262
+"""
 
 
-def overwrite(data: Matrix) -> Matrix:
+def overwrite_nan(data: Matrix) -> Matrix:
     """
-    If there is NaN in the i-th data, 
-    this function overwrites it with the most recent without-NaN data.
+    Overwrites NaN-vlaued data with the most recent without-NaN data.
 
     Parameters:
-        data: The data that you want to overwrite
+        data: The data that you want to overwrite.
     """
     for i in range(1, data.shape[0]):
         if np.any(np.isnan(data[i])):
             data[i] = data[i - 1]
+    return data
+
+
+def drop_anomaly(data: Matrix) -> Matrix:
+    """
+    Drops anomalies of the training data.
+
+    Parameters:
+        data: The data that you want to drop anomaly.
+
+    Retruns:
+        The data with dropped anomaly.
+    """
+    new_data = []
+    for i in range(1, data.shape[0]):
+        if data[i, -1] == 0:
+            new_data.append(data[i])
+    new_data = np.stack(new_data)
+    return new_data
+
+
+def overwrite_anomaly(data: Matrix) -> Matrix:
+    """
+    Overwrites anomalies of the training data with the most recent normal data.
+
+    Parameters:
+        data: The data that you want to overwrite.
+
+    Returns:
+        The data that the anomaly is overwritten.
+    """
+    data = data.copy()
+    last_normal = data[0].copy()
+    for i in range(1, data.shape[0]):
+        if data[i, -1] == 1:
+            data[i] = last_normal
+        else:
+            last_normal = data[i].copy()
     return data
 
 
@@ -27,21 +70,27 @@ class Dataset(object):
     Load data.
 
     Parameters:
-        dataset:     Name of the dataset.
-        window_size: Length of the sliding window. Default is 100.
-        mode:        Either train or test.
-        convert_nan: How to convert the data with NaN value. Default 'nan_to_zero.'
-                     If dataset is 'GECCO_2018' or 'CECCO_2019', then set it to 'overwrite.' 
-        train_traio: The ratio of train set. For MSL, SMAP, SMD, SWaT, it is unnecessary.
-                     For the GECCO_2018 and GECCO_2019, default is 0.8.
+        dataset:            Name  of the dataset.
+        window_size:        Length of the sliding window.
+                            Following the paper, default is 200.
+        mode:               Either train or test.
+
+        convert_nan:        How to convert the data with NaN value. Default 'nan_to_zero.'
+                            If dataset is 'GECCO_2018' or 'CECCO_2019', then set it to 'overwrite.'
+
+        anomaly_processing: How to process anomalies of the training dataset. Default is 'drop.'
+
+        train_traio:        The ratio of train set. For MSL, SMAP, SMD, SWaT, it is unnecessary.
+                            For GECCO_2018 and GECCO_2019, default is 0.5, following Appendix A of the paper.
     """
     def __init__(
         self,
         dataset: str,
-        window_size: int = 100,
+        window_size: int = 200,
         mode: str = 'train',
         convert_nan: str = 'nan_to_zero',
-        train_ratio: float = 0.8,
+        anomaly_processing: str = 'drop',
+        train_ratio: float = 0.5,
     ) -> None:
         data_path = os.path.join(PATH, dataset)
         scaler = StandardScaler()
@@ -70,6 +119,12 @@ class Dataset(object):
         elif dataset in ['GECCO_2018', 'GECCO_2019']:
             data = pd.read_csv(os.path.join(PATH, 'GECCO', dataset, f'1_{dataset.lower().replace('_', '')}_water_quality.csv'))
             data.drop(columns=['Time'], inplace=True)
+            
+            if anomaly_processing == 'drop':
+                data = drop_anomaly(data)
+            elif anomaly_processing == 'overwrite':
+                data = overwrite_anomaly(data)
+            
             data = data.values[:, 1:]
             labels = data[:, -1]
             labels = np.where(labels == False, 0, 1)
@@ -82,7 +137,7 @@ class Dataset(object):
                 labels = labels[train_size:]
         
         if convert_nan == 'overwrite':
-            data = overwrite(data)
+            data = overwrite_nan(data)
         elif convert_nan == 'nan_to_zero':
             data = np.nan_to_num(data)
 
