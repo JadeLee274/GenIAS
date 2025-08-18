976c91e8c76b31402265460107d86a32821ba947
JadeLee274 <tmdals274@snu.ac.kr>
2025-08-18 16:07:12 +0900

    modify loss for new model structure

--- utils/loss.py
+++ utils/loss.py
@@ -1,4 +1,5 @@
 from typing import *
+from math import log, sqrt
 import numpy as np
 import torch
 from torch import Tensor
@@ -9,6 +10,8 @@ Codes for loss function of TCN-VAE. This code follows the paper
 Darban et al., 2025, GenIAS: Generator for Instantiating Anomalies in Time Series.
 
 Paper link: https://arxiv.org/pdf/2502.08262
+
+Default values of arguments follow the paper.
 """
 
 
@@ -44,17 +47,12 @@ def pert_loss(
         x_hat:     Reconstrucded data, without anomalies injected.
         x_tilde:   Reconstructed data, with anomalies injected.
 
-        delta_min: Minmum threshold of recon(x, x_hat) - recon(x, x_tilde)
-                   Following the paper, default is 0.1.
+        delta_min: Minmum threshold of recon(x, x_hat) - recon(x, x_tilde). Default is 0.1.
 
-        delta_max: Maxmum threshold of recon(x, x_tilde)
-                   Following the paper, default is 0.2.
+        delta_max: Maxmum threshold of recon(x, x_tilde). Default is 0.2.
 
     Returns:
         Perturbation loss.
-
-    Here, the defaults of delta_min and delta_max is set to 0.1 and 0.2,
-    following section 4.8.3 of the paper.
     """
     return F.relu(recon_loss(x, x_hat) - recon_loss(x, x_tilde) + delta_min) \
     + F.relu(recon_loss(x, x_tilde) - delta_max)
@@ -72,7 +70,8 @@ def zero_pert_loss(x: Tensor, x_tilde: Tensor) -> Tensor:
     Returns:
         Zero perturbation loss.
     """
-    return torch.mean((recon_loss(x, x_tilde) + 1) ** -1)
+    ind = torch.where(torch.isclose(x, torch.zeros_like(x)).any(dim=2))
+    return torch.mean((recon_loss(x[ind[0], ind[1]], x_tilde[ind[0], ind[1]]) + 1) ** -1)
 
 
 def kld_loss(
@@ -97,10 +96,10 @@ def kld_loss(
     """
     return torch.mean(
         -0.5 * torch.sum(
-            input=(1 + logvar - mu ** 2 - torch.exp(logvar)/prior_var + 2 * prior_var),
-            dim=1,
+            input=(1 + logvar - mu**2 - torch.exp(logvar)/prior_var + 2*log(sqrt(prior_var))),
+            dim=2,
         ),
-    dim=0,
+    dim=[0, 1],
     )
 
 
@@ -110,7 +109,7 @@ def loss(
     x_tilde: Tensor,
     mu: Tensor,
     logvar: Tensor,
-    prior_var: float = 0.5,
+    prior_var: float = 0.579,
     recon_weight: float = 1.0,
     pert_weight: float = 0.1,
     zero_pert_weight: float = 0.01,
@@ -125,31 +124,19 @@ def loss(
         x_tilde:          Perturbed output, from the VAE.
         mu:               Mean of latent space, from the encoder.
         logvar:           Log variance of latent space, from the encoder.
-
-        prior_var:        Prior variance prior of the latent space.
-                          Following the paper, default is 0.5.
-
-        recon_weight:     Weight of the reconstruction loss.
-                          Following the paper, default is 1.0.
-                          
-        pert_weight:      Weight of the perturbation loss.
-                          Following the paper, default is 0.1.
-                          
-        zero_pert_weight: Weight of the zero perturbation loss.
-                          Following the paper, default is 0.01.
+        prior_var:        Prior variance prior of the latent space. Default is 0.5.
+        recon_weight:     Weight of the reconstruction loss. Default is 1.0.
+        pert_weight:      Weight of the perturbation loss. Default is 0.1.
+        zero_pert_weight: Weight of the zero perturbation loss. Default is 0.01.
                           It the dataset if univariate dataset, set it to 0.0.
-
-        kld_weight:       Weight of the kld loss.
-                          Following the paper, default is 0.1.
+        kld_weight:       Weight of the kld loss. Default is 0.1.
 
     Returns:
-        Total loss of VAE.
-
-    Here, the defaults of recon_weight, pert_weight, zero_pert_weight, kld_weight
-    are given by 1.0., 0.1, 0.01, 0.1, following section 4.4 of the paper.
+        Reconstruction loss, Perturbatino loss, Zero perturbation loss, KL-Divergence loss, and Total loss of VAE.
+        All losses except the total loss are detached. They are only for recording loss during training.
     """
     recon = recon_loss(x, x_hat)
-    pert = pert_loss(x, x_hat)
+    pert = pert_loss(x, x_hat, x_tilde)
     zero_pert = zero_pert_loss(x, x_tilde)
     kld = kld_loss(mu, logvar, prior_var)
     total = recon_weight * recon + pert_weight * pert + zero_pert_weight * zero_pert + kld_weight * kld
