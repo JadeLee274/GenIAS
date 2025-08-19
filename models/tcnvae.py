dba567dbda94f718bc6c152dcaee3c7e6c45db84
JadeLee274 <tmdals274@snu.ac.kr>
2025-08-19 20:45:39 +0900

    remove old structure

--- models/tcnvae.py
+++ models/tcnvae.py
@@ -15,95 +15,6 @@ Default values of arguments follow the paper.
 """
 
 
-# class Encoder(nn.Module):
-#     """
-#     TCN-based Encoder layer of VAE.
-#     Given the input with shape (B, W, F), where
-    
-#     B = Batch size,
-#     W = Window size,
-#     F = Number of features of each data point,
-
-#     the parameters of encoder is given as follows.
-
-#     Parameters:
-#         window_size:    W
-#         num_features:   F
-#         tcn_depth:      Depth of the TCN layer.
-#         hidden_list:    List of the in_channels of each TNC layers.
-#                         It decides the number of TCN layers.
-#                         Given hidden_list as [h_1, h_2, ..., 1], the input with
-#                         shape (B, W, F) is transposed to (B, F, W), and is
-#                         forwarded by the TCN layers to shape (B, 1, W). 
-#         dropout:        In what probabiliy that the dropout layer of eacn TCN layer
-#                         will be activated.
-#         latent_dim:     The dimension of latent space. The input forwarded by 
-#                         the TNC layers with shape (B, 1, W) is squeezed to (B, W),
-#                         and is forwarded by fc_mu and fc_logvar to shapes (B, L) 
-#                         and (B, L).
-#     """
-#     def __init__(
-#         self,
-#         window_size: int,
-#         num_features: int,
-#         tcn_depth: int,
-#         hidden_list: List[int],
-#         dropout: float,
-#         latent_dim: int,
-#     ) -> None:
-#         super().__init__()
-#         enc_depth = len(hidden_list)
-
-#         enc_layer = []
-#         activation = nn.ReLU()
-
-#         for i in range(enc_depth - 1):
-#             in_channels = num_features if i == 0 else hidden_list[i - 1]
-#             out_channels = hidden_list[i]
-#             enc_layer.append(
-#                 TemporalConvNet(
-#                     in_channels=in_channels,
-#                     hidden_channels=[out_channels] * tcn_depth,
-#                 )
-#             )
-#             enc_layer.append(activation)
-#             enc_layer.append(nn.Dropout(p=dropout))
-        
-#         enc_layer.append(
-#             TemporalConvNet(
-#                 in_channels=hidden_list[enc_depth - 2],
-#                 hidden_channels=[hidden_list[enc_depth - 1]] * tcn_depth,
-#             )
-#         )
-#         enc_layer.append(activation)
-#         self.enc_layer = nn.Sequential(*enc_layer)
-
-#         self.fc_mu = nn.Linear(
-#             in_features=window_size,
-#             out_features=latent_dim,
-#         )
-#         self.fc_logvar = nn.Linear(
-#             in_features=window_size,
-#             out_features=latent_dim,
-#         )
-
-#         self._init_linear_weights()
-    
-#     def forward(self, x: Tensor) -> Tuple[Tensor, Tensor]:
-#         x = x.transpose(1, 2)      # B, W, F -> B, F, W
-#         x = self.enc_layer(x)      # B, F, W -> B, 1, W
-#         x = x.squeeze(1)           # B, 1, W -> B, W
-#         mu = self.fc_mu(x)         # B, W    -> B, L
-#         logvar = self.fc_logvar(x) # B, W    -> B, L
-#         return mu, logvar
-
-#     def _init_linear_weights(self) -> None:
-#         for m in self.parameters():
-#             if isinstance(m, nn.Linear):
-#                 nn.init.xavier_normal_(m.weight)
-#                 nn.init.zeros_(m.bias)
-
-
 class Encoder(nn.Module):
     def __init__(
         self,
@@ -133,10 +44,10 @@ class Encoder(nn.Module):
         nn.init.zeros_(self.fc_logvar.bias)
 
     def forward(self, x: Tensor) -> Tuple[Tensor, Tensor]:
-        x = self.encoder(x) # (B, W, F) -> (B, W, F)
-        mu = self.fc_mu(x) # (B, W, F) -> (B, W, L)
-        logvar = self.fc_logvar(x) # (B, W, F) -> (B, W, L)
-        return mu, logvar # (B, W, L)
+        x = self.encoder(x) # (B, W, F) -> (B, 1, F)
+        mu = self.fc_mu(x) # (B, 1, F) -> (B, 1, L)
+        logvar = self.fc_logvar(x) # (B, 1, F) -> (B, 1, L)
+        return mu, logvar # (B, 1, L), (B, 1, L)
 
 
 class Decoder(nn.Module):
@@ -218,77 +129,6 @@ class Decoder(nn.Module):
             elif isinstance(m, nn.ConvTranspose1d):
                 nn.init.kaiming_normal_(m.weight)
                 nn.init.zeros_(m.bias)
-
-
-# class VAE(nn.Module):
-#     """
-#     TCN-based VAE. Uses Encoder and Decoder classes for encoder and decoder
-#     layers. Given the input with shape (B, W, F), where 
-
-#     B = Batch size,
-#     W = Window length,
-#     F = Number of features of each data point,
-
-#     the parameters are given as follows.
-
-#     Parameters:
-#         window_size:   W
-#         data_dim:      F
-#         latent_dim:    Dimension of latent space.
-#         hidden_list:   List of the in_channels of each TNC layers of encoder.
-#                        It decides the depth of encoder and decoder.
-#                        The hidden_list of decoder is the reversed version of it.
-#         tcn_depth:     Depth of the TCN layer.
-#         perturb_const: Perturbation constant for the perturbation in the latent space.
-#     """
-#     def __init__(
-#         self,
-#         window_size: int,
-#         data_dim: int,
-#         latent_dim: int,
-#         hidden_list: List[int],
-#         tcn_depth: int,
-#     ) -> None:
-#         super().__init__()
-#         self.encoder = Encoder(
-#             window_size=window_size,
-#             num_features=data_dim,
-#             tcn_depth=tcn_depth,
-#             hidden_list=hidden_list,
-#             dropout=0.1,
-#             latent_dim=latent_dim,
-#         )
-#         self.decoder = Decoder(
-#             latent_dim=latent_dim,
-#             window_size=window_size,
-#             num_features=data_dim,
-#             hidden_list=list(reversed(hidden_list)),
-#             dropout=0.1,
-#         )
-#         self.psi = nn.Parameter(
-#             data=torch.ones(1, latent_dim),
-#             requires_grad=True,
-#         )
-    
-#     def reparam_and_perturb(
-#         self,
-#         mu: Tensor,
-#         logvar: Tensor,
-#         psi: nn.Parameter,
-#     ) -> Tuple[Tensor, Tensor]:
-#         eps = torch.randn_like(mu)
-#         sigma = torch.exp(0.5 * logvar)
-#         z_recon = mu + eps * sigma
-#         z_pert = mu + psi * eps * sigma
-#         return z_recon, z_pert
-    
-#     def forward(self, x: Tensor) -> Tuple[Tensor, Tensor]:
-#         mu, logvar = self.encoder(x)
-#         z_recon, z_pert = self.reparam_and_perturb(mu, logvar, psi=self.psi)
-#         x_hat = self.decoder(z_recon)
-#         x_tilde = self.decoder(z_pert)
-#         return mu, logvar, x_hat, x_tilde
-
 
 
 class VAE(nn.Module):
