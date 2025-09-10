4b26f8a74487529716a8476f82215e942f95fe89
JadeLee274 <tmdals274@snu.ac.kr>
2025-09-10 19:21:53 +0900

    now anchor and negative pair are not included in their nearest neighborhood

--- carla_main.py
+++ carla_main.py
@@ -202,7 +202,7 @@ def pretext(
     negative_reps = torch.cat(negative_reps, dim=0).numpy()
     
     reps = np.concatenate([anchor_reps, negative_reps], axis=0)
-    num_negative_reps = reps.shape[0] // 2
+    num_negative_reps = negative_reps.shape[0]
 
     index_searcher = IndexFlatL2(reps.shape[1])
     index_searcher.add(reps)
