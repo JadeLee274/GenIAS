f85f18a645c13b57769e490cf1e977568a347feb
JadeLee274 <tmdals274@snu.ac.kr>
2025-09-15 15:45:25 +0900

    [MODIFY] fix typeo

--- utils/metric.py
+++ utils/metric.py
@@ -94,12 +94,7 @@ def macro_f1(f1_list: List[float]) -> float:
     Returns:
         Macro F1 score.     
     """
-<<<<<<< HEAD
-    assert len(f1_list) > 1, 'This funcation is for dataset with multiple data' 
-=======
-    assert len(f1_list) > 1, \
-    'This funcation is for dataset with multiple data' 
->>>>>>> main
+    assert len(f1_list) > 1, 'This function is for dataset with multiple data' 
     return round(np.mean(f1_list), 4)
 
 
