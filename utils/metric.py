e4b1749234b51457366cdea8c672cd375384dca9
JadeLee274 <tmdals274@snu.ac.kr>
2025-09-18 22:57:52 +0900

    [ADD] add inference function

--- utils/metric.py
+++ utils/metric.py
@@ -1,4 +1,7 @@
+from sklearn.metrics import precision_recall_curve, auc
 from utils.common_import import *
+from data_factory.loader import ClassificationDataset
+from carla.model import ClassificationModel
 
 
 ######################### Metrics for CARLA inference ######################### 
@@ -113,6 +116,128 @@ def auc_pr_statistics(auc_pr_list: List[float]) -> Tuple[float, float]:
     auc_pr_mean = round(np.mean(auc_pr_list), 4)
     auc_pr_std = round(np.std(auc_pr_list), 4)
     return auc_pr_mean, auc_pr_std
+
+
+def inference(dataset: str, gpu_num: int, pretext_scheme: str) -> None:
+    """
+    If you only want to infer scores with pre-trained models, by using this 
+    function, you can get informations on F1 scores, AUC-PR.
+
+    Parameters:
+        dataset: Name of dataset.
+        gpu_num: The inference will be done on this GPU.
+        pretext_scheme: What scheme was applied for pretext.
+    """
+    assert pretext_scheme in ['carla', 'genias', 'shuffle'], \
+    "pretext_scheme must be either 'carla', 'genias', 'shuffle'"
+
+    device = torch.device(f'cuda:{gpu_num}')
+
+    best_f1_list = []
+    best_tp_list = []
+    best_fp_list = []
+    best_fn_list = []
+    auc_pr_list = []
+
+    data_list = sorted(os.listdir(f'data/{dataset}/test'))
+    data_list = [f.replace('.npy', '') for f in data_list]
+
+    for subdata in data_list:
+        test_set = ClassificationDataset(
+            dataset=dataset,
+            subdata=subdata,
+            mode='test',
+            pretext_scheme='carla',
+        )
+        data_dim = test_set.data_dim
+        model = ClassificationModel(in_channels=data_dim)
+        ckpt_path = os.path.join(
+            'checkpoints', 'classification', dataset, subdata, pretext_scheme
+        )
+        ckpt = torch.load(os.path.join(ckpt_path, 'epoch_100.pt'))
+        model.load_state_dict(ckpt['model'])
+        model = model.to(device)
+
+        logits = []
+
+        for test_data in test_set:
+            test_data = torch.tensor(test_data).float().to(device)
+            logit = model.forward(test_data.unsqueeze(0).transpose(-2, -1))
+            logit = logit.squeeze(0).detach().cpu().numpy()
+            logits.append(logit)
+        
+        classes = [0 for _ in range(10)]
+
+        for logit in logits:
+            max_idx = np.argmax(logit)
+            classes[max_idx] += 1
+        
+        major_class = classes.index(max(classes))
+
+        anomaly_scores = []
+
+        for logit in logits:
+            major_probability = logit[major_class]
+            anomaly_scores.append(1 - major_probability)
+        
+        anomaly_scores = np.array(anomaly_scores)
+
+        precision, recall, thresholds = precision_recall_curve(
+            y_true=test_set.labels,
+            y_score=anomaly_scores,
+        )
+
+        auc_pr = auc(recall, precision)
+
+        best_threshold = 0
+        best_f1 = 0
+
+        for i in range(len(thresholds)):
+            f1_score = f1score(precision[i], recall[i])
+            if f1_score > best_f1:
+                best_f1 = f1_score
+                best_threshold = thresholds[i]
+        
+        best_anomaly_prediction = np.where(
+            anomaly_scores >= best_threshold, 1, 0
+        )
+
+        best_f1_score, best_tp, best_fp, best_fn = f1_stat(
+            prediction=best_anomaly_prediction,
+            gt=test_set.labels,
+        )
+
+        best_f1_list.append(best_f1_score)
+        best_tp_list.append(best_tp)
+        best_fp_list.append(best_fp)
+        best_fn_list.append(best_fn)
+        auc_pr_list.append(auc_pr)
+    
+    best_f1_list = np.array(best_f1_list)
+    best_tp_list = np.array(best_tp_list)
+    best_fp_list = np.array(best_fp_list)
+    best_fn_list = np.array(best_fn_list)
+    auc_pr_list = np.array(auc_pr_list)
+
+    f1_score_best = np.max(best_f1_list)
+    precision, recall, f1_micro = mirco_f1(
+        tp_list=best_tp_list,
+        fp_list=best_fp_list,
+        fn_list=best_fn_list
+    )    
+    auc_pr_mean = np.mean(auc_pr_list)
+    auc_pr_std = np.std(auc_pr_list)
+    f1_macro = macro_f1(f1_list=best_f1_list)
+
+    print(f'- Best F1: {round(f1_score_best, 4)}')
+    print(f'- Micro F1: {round(f1_micro, 4)}')
+    print(f'- Precision: {round(precision, 4)}')
+    print(f'- Recall: {round(recall, 4)}')
+    print(f'- AUC-PR mean: {round(auc_pr_mean, 4)}')
+    print(f'- AUC-PR std: {round(auc_pr_std, 4)}')
+    print(f'- Macro F1: {round(f1_macro, 4)}')
+
+    return
 
 
 ####################### Metrics for anomaly generation ####################### 
