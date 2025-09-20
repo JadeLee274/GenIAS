eb36802f665d658af2d78d797e85dc968056b665
JadeLee274 <tmdals274@snu.ac.kr>
2025-09-20 23:14:51 +0900

    [ADD] add modes to seed fixing function

--- utils/fix_seed.py
+++ utils/fix_seed.py
@@ -2,12 +2,24 @@ import random
 import numpy as np
 import torch
 
-def fix_seed_all(seed: int) -> None:
+def fix_seed(seed: int = 42, mode: str = 'all') -> None:
     '''
     Fix seed for experiment reproduction. (random, numpy, torch)
+
+    Parameters:
+        seed: Seed number. Default 42.
+        mode: Which library that the seed fixing will be applied. Default 'all'
     '''
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
+    if mode == 'random':
+        random.seed(seed)
+    elif mode == 'numpy':
+        np.random.seed(seed)
+    elif mode== 'torch':
+        torch.manual_seed(seed)
+        torch.backends.cudnn.deterministic = True
+    elif mode == 'all':
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.random.seed(seed)
+        torch.backends.cudnn.deterministic = True
     return None
